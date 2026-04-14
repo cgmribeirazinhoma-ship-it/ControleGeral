09b27fedde71799e80df538fca32e4415f381d64
cgmribeirazinhoma-ship-it <cgmribeirazinho.ma@gmail.com>
2026-04-14 00:39:53 -0300

    Delete tests directory

--- tests/bug-2-docx-version.test.js
+++
@@ -1,8 +0,0 @@
-// Test file for bug 2 related to docx version functionality
-
-describe('DOCX Version Bug 2 Tests', () => {
-    test('should handle specific scenarios for DOCX version properly', () => {
-        // Your test implementation here
-        expect(true).toBe(true); // Placeholder
-    });
-});
